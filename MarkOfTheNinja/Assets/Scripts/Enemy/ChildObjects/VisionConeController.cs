56bd94a93ae7bb267c933f22d12734b033cf1a7d
mateoCosta1 <89783683+mateoCosta1@users.noreply.github.com>
2024-10-31 15:32:28 -0300

    new vision cone v1

--- MarkOfTheNinja/Assets/Scripts/Enemy/ChildObjects/VisionConeController.cs
+++ MarkOfTheNinja/Assets/Scripts/Enemy/ChildObjects/VisionConeController.cs
@@ -7,11 +7,14 @@ public class VisionConeController : MonoBehaviour
 {
     // Start is called before the first frame update
     private readonly string playerTag = "Player";
+    private readonly string groundLayer = "Ground";
+    private readonly string wallLayer = "Walls";
+    private readonly string visionOriginName = "VisionOrigin";
     private bool playerIsBeingSeen = false;
     private ILevelManager levelManager;
     private float lastDistance;
     private EnemyController enemy;
-
+    private GameObject eyes;
     private void OnTriggerStay2D(Collider2D collision)
     {
         PlayerWasSeen(collision);
@@ -29,7 +32,8 @@ public class VisionConeController : MonoBehaviour
     }
     void Start()
     {
-        
+        Transform eyesTransform = transform.Find(visionOriginName);
+        eyes = eyesTransform.gameObject;
     }
 
     // Update is called once per frame
@@ -46,7 +50,7 @@ public class VisionConeController : MonoBehaviour
         if (!collision.gameObject.CompareTag(playerTag)) return;
         var enemy = GetComponentInParent<EnemyController>();
         this.enemy = enemy;
-        if (ObjectDetector.AnyObjectsBetween(enemy.gameObject, collision)) return;
+        if (ObjectDetector.AnyObjectsBetween(eyes, collision, new string[] {groundLayer,wallLayer})) return;
         var enemyPosition = enemy.transform.position;
         var playerPosition = collision.gameObject.transform.position;
         float distance = Vector2.Distance(enemyPosition, playerPosition);
@@ -60,7 +64,7 @@ public class VisionConeController : MonoBehaviour
     {
         if (!collision.gameObject.CompareTag(playerTag)) return;
         var enemy = GetComponentInParent<EnemyController>();
-        if (ObjectDetector.AnyObjectsBetween(enemy.gameObject, collision) && !playerIsBeingSeen) return;
+        if (ObjectDetector.AnyObjectsBetween(enemy.gameObject, collision, new string[] { groundLayer, wallLayer }) && !playerIsBeingSeen) return;
         playerIsBeingSeen = false;
         enemy.PlayerLeftVisionRadius(collision.gameObject.transform.position);
     }

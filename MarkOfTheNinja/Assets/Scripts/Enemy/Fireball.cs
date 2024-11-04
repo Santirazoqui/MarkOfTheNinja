f01386fc84c57b6af7a0818a609da9f6e792d6f4
Santirazoqui <71331155+Santirazoqui@users.noreply.github.com>
2024-11-04 12:13:41 -0300

    fireball

--- MarkOfTheNinja/Assets/Scripts/Enemy/Fireball.cs
+++ MarkOfTheNinja/Assets/Scripts/Enemy/Fireball.cs
@@ -1,9 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Fireball : MonoBehaviour
 {
+    [SerializeField] Transform target;
+    NavMeshAgent agent;
+
+    void Start() 
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    void Update()
+    {
+        agent.SetDestination(target.position);
+        //transform.LookAt(target, Vector3.forward);
+
+        Vector3 dir = target.position - transform.position;
+        float angle = Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        // Vector3 relativePos = target.position - transform.position;
+        // transform.rotation = Quaternion.LookRotation(relativePos, Vector2.up);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))

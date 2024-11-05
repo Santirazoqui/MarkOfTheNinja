c86f3c4cf0281b80535784020a29fa3178efc17b
mateoCosta1 <89783683+mateoCosta1@users.noreply.github.com>
2024-11-04 22:53:32 -0300

    player animation v1 done

--- MarkOfTheNinja/Assets/Scripts/Scripts/PlayerAnimationController.cs
+++ MarkOfTheNinja/Assets/Scripts/Scripts/PlayerAnimationController.cs
@@ -10,40 +10,49 @@ namespace Assets.Scripts.Player
     public class PlayerAnimationController
     {
         private readonly Animator animator;
+        private enum AnimationStates
+        {
+            Still,
+            Running,
+            Jumping,
+            OnAir,
+            Landing,
+            Dashing,
+        }
+
+        private readonly Dictionary<AnimationStates, string> mapper = new() {
+            { AnimationStates.Still, "idle" },
+            { AnimationStates.Running, "running" },
+            { AnimationStates.Jumping, "jumping" },
+            { AnimationStates.OnAir, "onAir"},
+            { AnimationStates.Landing, "landing"},
+            { AnimationStates.Dashing, "dashing" },
+        };
 
         public PlayerAnimationController(Animator animator)
         {  
             this.animator = animator; 
         }
 
-        public void Dash()
+        public void Dash() => SetAnimationState(AnimationStates.Dashing);
+
+        public void Idle() => SetAnimationState(AnimationStates.Still);
+
+        public void Running() => SetAnimationState(AnimationStates.Running);
+
+        public void JumpStart() => SetAnimationState(AnimationStates.Jumping);
+
+        public void OnAir() => SetAnimationState(AnimationStates.OnAir);
+
+        public void Landed() => SetAnimationState(AnimationStates.Landing);
+
+        private void SetAnimationState(AnimationStates state)
         {
-
-        }
-
-        public void Idle() 
-        { 
-        
-        }
-
-        public void Running()
-        {
-
-        }
-
-        public void JumpStart()
-        {
-
-        }
-
-        public void OnAir()
-        {
-
-        }
-
-        public void Landed()
-        {
-
+            foreach (var (s, n) in mapper)
+            {
+                if (s == state) animator.SetTrigger(n);
+                else animator.ResetTrigger(n);
+            }
         }
     }
 }

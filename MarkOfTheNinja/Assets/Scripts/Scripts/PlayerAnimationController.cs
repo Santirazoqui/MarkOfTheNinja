1381c6167cd558e50247c521c17ed8665a51514b
mateoCosta1 <89783683+mateoCosta1@users.noreply.github.com>
2024-11-04 21:51:29 -0300

    animationsInAnimatorImplemented

---
+++ MarkOfTheNinja/Assets/Scripts/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class PlayerAnimationController
+    {
+        private readonly Animator animator;
+
+        public PlayerAnimationController(Animator animator)
+        {  
+            this.animator = animator; 
+        }
+
+        public void Dash()
+        {
+
+        }
+
+        public void Idle() 
+        { 
+        
+        }
+
+        public void Running()
+        {
+
+        }
+
+        public void JumpStart()
+        {
+
+        }
+
+        public void OnAir()
+        {
+
+        }
+
+        public void Landed()
+        {
+
+        }
+    }
+}

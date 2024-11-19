6c702310bffc4ba58fdd58a832b6bbe5f1d486db
Santirazoqui <71331155+Santirazoqui@users.noreply.github.com>
2024-11-19 01:06:57 -0300

    create spike tilemap prefab

--- MarkOfTheNinja/Assets/Scripts/Zenject/DependencyInjectionInstaller.cs
+++ MarkOfTheNinja/Assets/Scripts/Zenject/DependencyInjectionInstaller.cs
@@ -1,12 +1,16 @@
 using Assets.Scripts.DataAccess;
 using UnityEngine;
 using Zenject;
+using UnityEngine.SceneManagement;
 
 public class DependencyInjectionInstaller : MonoInstaller
 {
     public override void InstallBindings()
     {
         Container.Bind<IDataAccessManager>().To<FileDataAccess>().AsSingle();
-        Container.Bind<ISceneSwitcher>().To<SceneSwitcher>().FromComponentInHierarchy().AsSingle();
+        if(SceneManager.GetActiveScene().name == "SL1-Mateo") {
+            Debug.Log("SL1-Mateo");
+            Container.Bind<ISceneSwitcher>().To<SceneSwitcher>().FromComponentInHierarchy().AsSingle();
+        }
     }
 }

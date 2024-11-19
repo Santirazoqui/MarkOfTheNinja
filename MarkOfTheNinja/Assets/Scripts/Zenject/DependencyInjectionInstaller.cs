e2c2daaf702499af985af11f13bf6ce71b9b276e
mateoCosta1 <89783683+mateoCosta1@users.noreply.github.com>
2024-11-19 13:48:33 -0300

    Added scene switcher to level (no me mates Santiago)

--- MarkOfTheNinja/Assets/Scripts/Zenject/DependencyInjectionInstaller.cs
+++ MarkOfTheNinja/Assets/Scripts/Zenject/DependencyInjectionInstaller.cs
@@ -8,9 +8,10 @@ public class DependencyInjectionInstaller : MonoInstaller
     public override void InstallBindings()
     {
         Container.Bind<IDataAccessManager>().To<FileDataAccess>().AsSingle();
+		Container.Bind<ISceneSwitcher>().To<SceneSwitcher>().FromComponentInHierarchy().AsSingle();
         if(SceneManager.GetActiveScene().name == "SL1-Mateo") {
             Debug.Log("SL1-Mateo");
-            Container.Bind<ISceneSwitcher>().To<SceneSwitcher>().FromComponentInHierarchy().AsSingle();
+            
         }
     }
 }

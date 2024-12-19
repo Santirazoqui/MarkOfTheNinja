4362571e14a9048c9409e9cba5a53d26916eaa2d
mateoCosta1 <89783683+mateoCosta1@users.noreply.github.com>
2024-12-18 21:32:33 -0300

    stump done

--- MarkOfTheNinja/Assets/Scripts/Enemy/ChildObjects/HeadHitboxScript.cs
+++ MarkOfTheNinja/Assets/Scripts/Enemy/ChildObjects/HeadHitboxScript.cs
@@ -6,12 +6,19 @@ namespace Assets.Scripts.Enemy.ChildObjects
 {
     public class HeadHitboxScript : MonoBehaviour
     {
+        public float secondsOfSlideHitbox;
         private bool playerIsOnTop;
         private EnemyController controller;
+        private BoxCollider2D boxCollider;
+        private GameObject slidePlayerOff;
+        private readonly string slidePlayerOffPrefabName = "SlideOffMeHitbox";
         // Use this for initialization
         void Start()
         {
             controller = GetComponentInParent<EnemyController>();
+            boxCollider = GetComponent<BoxCollider2D>();
+            slidePlayerOff = transform.Find(slidePlayerOffPrefabName).gameObject;
+            slidePlayerOff.SetActive(false);
         }
 
         // Update is called once per frame
@@ -22,8 +29,19 @@ namespace Assets.Scripts.Enemy.ChildObjects
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            if (!Util.Util.CollidedWithPlayer(collision)) return;
+            if (!Util.Util.CollidedWithPlayer(collision) || !boxCollider.enabled) return;
+            StopAllCoroutines();
             controller.ChangeStates(EnemyStates.Stunned);
+            StartCoroutine(SlideOff());
+        }
+
+        private IEnumerator SlideOff()
+        {
+            boxCollider.enabled = false;
+            slidePlayerOff.SetActive(true);
+            yield return new WaitForSeconds(secondsOfSlideHitbox);
+            boxCollider.enabled = true;
+            slidePlayerOff.SetActive(false);
         }
     }
 }

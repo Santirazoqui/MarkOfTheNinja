c421081fdfdb5c7d532e2d6ca9105efb5184036b
mateoCosta1 <89783683+mateoCosta1@users.noreply.github.com>
2024-12-23 15:43:10 -0300

    change to script otherwise build would fail

--- MarkOfTheNinja/Assets/Scripts/Enemy/States/NonDetectedState.cs
+++ MarkOfTheNinja/Assets/Scripts/Enemy/States/NonDetectedState.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Unity.VisualScripting;
-using UnityEditor.Tilemaps;
 using UnityEngine;
 
 namespace Assets.Scripts.Enemy.States

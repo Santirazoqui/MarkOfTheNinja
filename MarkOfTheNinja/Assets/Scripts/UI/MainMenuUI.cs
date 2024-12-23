ffdef6cf0634acaa79ef7ae34b5a15638e7ab74a
Santirazoqui <71331155+Santirazoqui@users.noreply.github.com>
2024-12-23 11:38:39 -0300

    change main menu

--- MarkOfTheNinja/Assets/Scripts/UI/MainMenuUI.cs
+++ MarkOfTheNinja/Assets/Scripts/UI/MainMenuUI.cs
@@ -6,21 +6,9 @@ using UnityEngine.SceneManagement;
 public class MainMenuUI : MonoBehaviour
 {
     [SerializeField] Canvas SettingsCanvas;
+    [SerializeField] Canvas LevelSelectCanvas;
     [SerializeField] AudioClip ButtonClickSound;
     [SerializeField] AudioSource audioSource;
-
-
-    public void Play()
-    {
-        PlayButtonSound();
-        SceneManager.LoadSceneAsync("BigLevel");
-    }
-
-    public void Tutorial()
-    {
-        PlayButtonSound();
-        SceneManager.LoadSceneAsync("Tutorial");
-    }
 
     public void OpenSettings()
     {
@@ -32,5 +20,29 @@ public class MainMenuUI : MonoBehaviour
     void PlayButtonSound()
     {
         audioSource.PlayOneShot(ButtonClickSound);
+    }
+
+    public void OpenLevelSelect()
+    {
+        this.gameObject.SetActive(false);
+        LevelSelectCanvas.gameObject.SetActive(true);
+    }
+
+    public void CloseLevelSelect()
+    {
+        LevelSelectCanvas.gameObject.SetActive(false);
+        this.gameObject.SetActive(true);
+    }
+
+    public void PlayHardLevel()
+    {
+        PlayButtonSound();
+        SceneManager.LoadSceneAsync("BigLevel");
+    }
+
+    public void PlayTutorial()
+    {
+        PlayButtonSound();
+        SceneManager.LoadSceneAsync("Tutorial");
     }
 }
